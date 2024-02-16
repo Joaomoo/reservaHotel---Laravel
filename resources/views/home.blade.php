b8020204b2e95e31f74c41cc8ab19e40db679a88
joaomoo <joaopedro232945@gmail.com>
2024-02-16 19:31:26 -0300

    fist commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO - Atividade Fudida de Difícil</title>
+    <link rel="shortcut icon" href="./images/zika.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <header>
+    O Tell
+
+    </header>
+
+    <main>
+            <h1>
+                H1 PIKA DAS IDEIAS
+            </h1>
+    </main>
+
+    <footer>
+            footer mais pika das ideias ainda
+    </footer>
+
+</body>
+</html>
